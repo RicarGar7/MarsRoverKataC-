82afadf2e0fe30d068ed068cb754d6443da6601c
ricargar7 <ricardo.garcia@leanmind.es>
2023-10-20 12:33:40 +0100

    🩹Fix test

--- Solution/Implementation/Rover.cs
+++ Solution/Implementation/Rover.cs
@@ -1,9 +1,13 @@
+using System.Runtime.InteropServices.ComTypes;
+
 namespace Test;
 
 public class Rover
 {
-    private Position _position;
-    private Facing _facing;
+    const int speed = 1;
+    
+    public Position _position;
+    public Facing _facing;
     private Map _map;
 
     public void LoadMap(Map map)
@@ -30,6 +34,35 @@ public class Rover
         
         _position = landPosition;
         _facing = facing;
+    }
+
+    public void Execute(Instructions instruction)
+    {
+        _position = instruction switch
+        {
+            Instructions.MoveForward when _facing == Facing.N => Position.Declare(_position._latitude + speed , _position._longitude),
+            Instructions.MoveForward when _facing == Facing.S => Position.Declare(_position._latitude - speed , _position._longitude),
+            Instructions.MoveForward when _facing == Facing.E => Position.Declare(_position._latitude, _position._longitude + speed),
+            Instructions.MoveForward when _facing == Facing.W => Position.Declare(_position._latitude, _position._longitude - speed),
+            Instructions.MoveBackwards when _facing == Facing.N => Position.Declare(_position._latitude - speed, _position._longitude),
+            Instructions.MoveBackwards when _facing == Facing.S => Position.Declare(_position._latitude + speed, _position._longitude),
+            Instructions.MoveBackwards when _facing == Facing.E => Position.Declare(_position._latitude, _position._longitude -speed),
+            Instructions.MoveBackwards when _facing == Facing.W => Position.Declare(_position._latitude, _position._longitude +speed),
+            _ => _position
+        };
+
+        _facing = instruction switch
+        {
+            Instructions.RotateLeft when _facing == Facing.N => _facing = Facing.W,
+            Instructions.RotateLeft when _facing == Facing.W => _facing = Facing.S,
+            Instructions.RotateLeft when _facing == Facing.S => _facing = Facing.E,
+            Instructions.RotateLeft when _facing == Facing.E => _facing = Facing.N,
+            Instructions.RotateRight when _facing == Facing.N => _facing = Facing.E,
+            Instructions.RotateRight when _facing == Facing.E => _facing = Facing.S,
+            Instructions.RotateRight when _facing == Facing.S => _facing = Facing.W,
+            Instructions.RotateRight when _facing == Facing.W => _facing = Facing.N,
+            _ => _facing
+        };
     }
 }
 
--- Solution/Tests/RoverTest.cs
+++ Solution/Tests/RoverTest.cs
@@ -2,6 +2,8 @@ namespace Test;
 
 public class RoverTest
 {
+    //Todo refactor to strategy pattern
+
     #region Landing
 
     [Fact]
@@ -50,4 +52,95 @@ public class RoverTest
     }
 
     #endregion
+
+    #region movement
+
+    [Fact]
+    public void Should_MoveAroundThePlanetSurface_MakingLinearMovements()
+    {
+        var planet = Planet.Create(new Surface(100, 100), new List<Obstacle>());
+        var rover = new Rover();
+        var satellite = new Satellite();
+        var map = satellite.Recognize(planet);
+        rover.LoadMap(map);
+        var initialPosition = Position.Declare(50, 50);
+        var initialFacing = Facing.N;
+        rover.Land(initialPosition, initialFacing);
+
+        rover.Execute(Instructions.MoveForward);
+
+        var expectedLatitude = initialPosition._latitude + 1;
+        Assert.Equal(rover._position._latitude,expectedLatitude);
+        Assert.Equal(rover._position._longitude,initialPosition._longitude);
+        
+        rover.Execute(Instructions.MoveBackwards);
+        Assert.Equal(rover._position._latitude,initialPosition._latitude);
+        Assert.Equal(rover._position._longitude,initialPosition._longitude);
+    }
+
+    [Fact]
+    public void Should_MoveAroundThePlanetSurface_MakingRotationalMovements()
+    {
+        var rover = LandRover(out _);
+
+        rover.Execute(Instructions.RotateLeft);
+        Assert.Equal(Facing.W,rover._facing);
+        rover.Execute(Instructions.RotateRight);
+        Assert.Equal(Facing.N,rover._facing);
+        rover.Execute(Instructions.RotateRight);
+        Assert.Equal(Facing.E,rover._facing);
+        rover.Execute(Instructions.RotateRight);
+        Assert.Equal(Facing.S,rover._facing);
+        rover.Execute(Instructions.RotateRight);
+        Assert.Equal(Facing.W,rover._facing);
+        rover.Execute(Instructions.RotateLeft);
+        Assert.Equal(Facing.S,rover._facing);
+        rover.Execute(Instructions.RotateLeft);
+        Assert.Equal(Facing.E,rover._facing);
+        rover.Execute(Instructions.RotateLeft);
+        Assert.Equal(Facing.N,rover._facing);
+    }
+
+    [Fact]
+    public void Should_MoveAroundThePlanetSurface_MakingBothRotationalAndLinearMovements()
+    {
+        var rover = LandRover(out var initialPosition);
+
+        rover.Execute(Instructions.RotateLeft);
+        rover.Execute(Instructions.MoveForward);
+
+        var expectedLongitude = initialPosition._longitude - 1;
+        Assert.Equal(rover._position._latitude,initialPosition._latitude);
+        Assert.Equal(rover._position._longitude,expectedLongitude);
+        
+        rover.Execute(Instructions.RotateRight);
+        rover.Execute(Instructions.MoveBackwards);
+
+        var expectedLatitude = initialPosition._latitude - 1;
+        Assert.Equal(rover._position._latitude,expectedLatitude);
+        Assert.Equal(rover._position._longitude,expectedLongitude);
+    }
+
+    private Rover LandRover(out Position initialPosition)
+    {
+        var planet = Planet.Create(new Surface(100, 100), new List<Obstacle>());
+        var rover = new Rover();
+        var satellite = new Satellite();
+        var map = satellite.Recognize(planet);
+        rover.LoadMap(map);
+        initialPosition = Position.Declare(50, 50);
+        var initialFacing = Facing.N;
+        rover.Land(initialPosition, initialFacing);
+        return rover;
+    }
+
+    #endregion
+}
+
+public enum Instructions
+{
+    MoveForward,
+    MoveBackwards,
+    RotateLeft,
+    RotateRight
 }
